e768a9ca92c47d162d80ab89ecbaa2ba222d8ab9
Taimakizel <taimakizel18@gmail.com>
2025-08-20 12:49:50 +0300

    C

--- admin.php
+++ admin.php
@@ -15,6 +15,7 @@
 <head>
     <meta charset="UTF-8">
     <title>לוח ניהול - חדר כושר</title>
+    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <style>
         body {
